2514a04838bfc88410b6654c431677dd0d45a4d4
Lalkrishna Yadav <sumitrayadav1455@gmail.com>
2020-12-19 12:41:06 +0530

    filechanged

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
+var constrainedLog;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
@@ -29,12 +31,15 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+    constrainedLog = new Log(230,180,80,PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,constrainedLog.body);
+    
 
 }
 
@@ -44,6 +49,10 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    
+    bird.display();
+    constrainedLog.display();
+    chain.display();
     box1.display();
     box2.display();
     ground.display();
@@ -58,8 +67,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
-    bird.display();
-
+    
     platform.display();
+
 }
